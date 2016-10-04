c3ce28e6ce354146c70ffeed22be8c4ad3669580
inveterateliterate <iribon9@gmail.com>
2016-10-04 11:02:20 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   resources :orders
   get "orders/new/:showing" => "orders#new", as: "new_by_showing"
   get "orders/movies/:id" => "orders#filter", as: "orders_by_movie"
-  resources :showings
+  
   get "showings/:date" => "showings#filter", as: "showings_by_date"
+  resources :showings
+  
 
   root 'movies#homepage'
   
