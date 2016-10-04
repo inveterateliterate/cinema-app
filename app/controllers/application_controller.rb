e84d20a44b844770f6f3437704e6f12787e557d4
inveterateliterate <iribon9@gmail.com>
2016-10-04 10:16:40 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  
+  def date_list
+    dates = []
+
+	  for i in 0..6
+		  if i === 0
+			  date = "Today, " + Date.today.strftime("%B %-d, %Y")
+			  dates.push(date)
+		  elsif i===1
+			  date = "Tomorrow, " + (Date.today + i).strftime("%B %-d, %Y")
+			  dates.push(date)
+		  else
+			  date = Date.today + i
+			  dates.push(date.strftime("%A, %B %-d, %Y"))
+		  end
+	  end
+	
+	  return dates 
+  end
 end
