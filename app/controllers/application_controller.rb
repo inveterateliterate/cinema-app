6ece38011bb962a2df7519fe6d7179941c8835d1
inveterateliterate <iribon9@gmail.com>
2016-10-04 10:50:32 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,27 @@ class ApplicationController < ActionController::Base
   
   def date_list
     dates = []
-
-	  for i in 0..6
-		  if i === 0
-			  date = "Today, " + Date.today.strftime("%B %-d, %Y")
-			  dates.push(date)
-		  elsif i===1
-			  date = "Tomorrow, " + (Date.today + i).strftime("%B %-d, %Y")
-			  dates.push(date)
-		  else
-			  date = Date.today + i
-			  dates.push(date.strftime("%A, %B %-d, %Y"))
-		  end
+	for i in 0..6
+		if i === 0
+			date = ["Today, ", Date.today.strftime("%B %-d, %Y")]
+			dates.push(date)
+		elsif i===1
+			date = ["Tomorrow, ", (Date.today + i).strftime("%B %-d, %Y")]
+			dates.push(date)
+		else
+			date = ["#{(Date.today + i).strftime("%A")}, ", (Date.today + i).strftime("%B %-d, %Y")]
+			dates.push(date)
+		end
 	  end
-	
-	  return dates 
+	return dates
   end
+	
+	def format_list
+		dates = date_list
+		list = []
+		dates.each do |x|
+			list.push(x.join(""))
+		end
+	end	
+	
 end
