651bbe2d79ce82523e74233ba6976e94dd4904a1
inveterateliterate <iribon9@gmail.com>
2016-10-04 11:31:35 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,17 @@ Rails.application.routes.draw do
   get "orders/new/:showing" => "orders#new", as: "new_by_showing"
   get "orders/movies/:id" => "orders#filter", as: "orders_by_movie"
   
+   root 'showings#homepage'
   get "showings/:date" => "showings#filter", as: "showings_by_date"
   resources :showings
   
 
-  root 'movies#homepage'
+ 
   
 
-
-  resources :movies
   get 'movies/:title' => "movies#show", as: "movie_by_title"
+  resources :movies
+  
   resources :auditoria
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
