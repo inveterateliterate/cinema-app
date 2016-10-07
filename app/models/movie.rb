d223e36d20509f8d2ea9ff66c37bc7f987a4de59
inveterateliterate <iribon9@gmail.com>
2016-10-07 09:27:17 -0500

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,8 @@ class Movie < ApplicationRecord
 	validates_presence_of :title, :runtime, :description, :rating
 	has_many :showings
 
-	has_attached_file :image, style: { medium: "300x300", thumb: "60x60" }, default_url: "https://pixabay.com/static/uploads/photo/2015/11/16/17/39/clouds-1046109_960_720.jpg"
-	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	#has_attached_file :image, style: { medium: "300x300", thumb: "60x60" }, default_url: "https://pixabay.com/static/uploads/photo/2015/11/16/17/39/clouds-1046109_960_720.jpg"
+	#validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 	def showings
   		Showing.where("movie_id=?", self.id)
